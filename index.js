b1b6fffdc79d2afdaa9f4d1d5f893cfcc35072f2
Shahin Sorkh <sorkh.shahin@hotmail.com>
2017-06-21 17:39:47 +0430

    Upload app

---
+++ index.js
@@ -0,0 +1,23 @@
+var fs = require('fs')
+
+var data = { patterns: [] }
+var sites = fs.readFileSync('domains').toString()
+
+sites = sites.split('\r\n')
+
+for (var site of sites) {
+  var pattern = {
+    enabled: true,
+    name: site,
+    pattern: "*" + site + "/*",
+    isRegEx: false,
+    caseSensitive: false,
+    blackList: false,
+    multiLine: false
+  }
+  data.patterns.push(pattern)
+}
+data = JSON.stringify(data)
+
+fs.writeFileSync('foxyproxy-patterns.json', data)
+console.log('done')

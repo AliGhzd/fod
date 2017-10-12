10f87da70ed4fdf0ad2487941dbcb69eb10eefb6
ShahinSorkh <sorkh.shahin@hotmail.com>
2017-10-12 14:55:01 +0330

    Update 2017-10-17

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-var fs = require('fs')
+let fs = require('fs')
 
-var data = { patterns: [] }
-var sites = fs.readFileSync('domains').toString()
+let data = { patterns: [] }
+let sites = fs.readFileSync('domains').toString()
 
 sites = sites.split('\n')
 
-for (var site of sites) {
-  var pattern = {
-    enabled: true,
-    name: site,
-    pattern: "*" + site + "/*",
-    isRegEx: false,
-    caseSensitive: false,
-    blackList: false,
-    multiLine: false
+for (let site of sites) {
+  if (site !== '') {
+    var pattern = {
+      enabled: true,
+      name: site,
+      pattern: '*' + site.substr(1) + '/*',
+      isRegEx: false,
+      caseSensitive: false,
+      blackList: false,
+      multiLine: false
+    }
+    data.patterns.push(pattern)
   }
-  data.patterns.push(pattern)
 }
 data = JSON.stringify(data)
 
